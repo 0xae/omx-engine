486e2b335155359876bb9fdf13818c7293992b4a
Ayrton Gomes <com.ayrton@gmail.com>
2019-01-30 22:33:26 -0100

    back to the game first commit on my new house ;) minor stuff

--- options/Random1.cpp
+++ options/Random1.cpp
@@ -17,36 +17,30 @@ using namespace std;
 
 
 
-double GetOneGaussianBySummation()
-{
+double GetOneGaussianBySummation() {
 	double result = 0;
     
-	for(unsigned long j=0;j<12;j++)
+	for(unsigned long j=0;j<12;j++) {
 		result += rand()/static_cast<double>(RAND_MAX);
-    
+	}
+
 	result -= 6.0;
-    
 	return result;
 }
 
 
-
-
-double GetOneGaussianByBoxMuller()
-{
+double GetOneGaussianByBoxMuller() {
 	double result;
 	double x, y;
 	double sizeSquared;
-    
-	do
-	{
+   
+	do {
 		x = 2.0 * rand()/static_cast<double>(RAND_MAX)-1;
 		y = 2.0 * rand()/static_cast<double>(RAND_MAX)-1;
 		sizeSquared = x*x + y*y;
-	}
-	while (sizeSquared >= 1.0);
-	result = x*sqrt(-2*log(sizeSquared)/sizeSquared);
-    
+	} while (sizeSquared >= 1.0);
+
+	result = x*sqrt(-2*log(sizeSquared)/sizeSquared);    
 	return result;
 }
 
