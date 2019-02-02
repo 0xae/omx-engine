acab948c8000b01b6aafcf0a753d475300b81481
Ayrton Gomes <com.ayrton@gmail.com>
2019-02-02 03:49:39 -0100

    first commit of saturday

--- options/Random1.cpp
+++ options/Random1.cpp
@@ -19,15 +19,13 @@ double GetOneGaussianBySummation() {
 
 double GetOneGaussianByBoxMuller() {
 	double result;
-	double x, y;
+	double x,y;
 	double sizeSquared;
-
 	do {
 		x = 2.0 * rand()/static_cast<double>(RAND_MAX)-1;
 		y = 2.0 * rand()/static_cast<double>(RAND_MAX)-1;
 		sizeSquared = x*x + y*y;
 	} while (sizeSquared >= 1.0);
-
 	result = x*sqrt(-2*log(sizeSquared)/sizeSquared);    
 	return result;
 }
