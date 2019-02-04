cb119ec009f5bcfbe749e8c3ecdbaf0e497e0d08
Ayrton Gomes <com.ayrton@gmail.com>
2019-02-04 15:11:37 -0100

    working on stuff

--- options/Random1.cpp
+++ options/Random1.cpp
@@ -8,11 +8,12 @@ using namespace std;
 double GetOneGaussianBySummation() {
 	double result = 0;
 
-	for(unsigned long j=0;j<12;j++) {
+	for (unsigned long j=0;j<12;j++) {
 		result += rand()/static_cast<double>(RAND_MAX);
 	}
 
 	result -= 6.0;
+
 	return result;
 }
 
@@ -21,11 +22,13 @@ double GetOneGaussianByBoxMuller() {
 	double result;
 	double x,y;
 	double sizeSquared;
+
 	do {
 		x = 2.0 * rand()/static_cast<double>(RAND_MAX)-1;
 		y = 2.0 * rand()/static_cast<double>(RAND_MAX)-1;
 		sizeSquared = x*x + y*y;
 	} while (sizeSquared >= 1.0);
+
 	result = x*sqrt(-2*log(sizeSquared)/sizeSquared);    
 	return result;
 }
