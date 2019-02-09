62c7183beda38f0963cc930f5bde0eaf89be4a16
Ayrton Gomes <com.ayrton@gmail.com>
2019-02-09 14:35:44 -0100

    working on stuff

--- options/Random1.cpp
+++ options/Random1.cpp
@@ -27,9 +27,7 @@ double GetOneGaussianByBoxMuller() {
 
 	do {
 		x = 2.0 * rand()/static_cast<double>(RAND_MAX)-1;
-
 		y = 2.0 * rand()/static_cast<double>(RAND_MAX)-1;
-
 		sizeSquared = x*x + y*y;
 
 	} while (sizeSquared >= 1.0);
