17c9fa9ab15931766a855a1d94d7916722457df2
Ayrton Gomes <com.ayrton@gmail.com>
2019-07-27 14:10:49 -0100

    remove existing logs from tracking repo

--- src/engine/marketprice/mp_config.h
+++ src/engine/marketprice/mp_config.h
@@ -13,28 +13,27 @@
 # include <ctype.h>
 # include <string.h>
 # include <stdlib.h>
-# include <unistd.h>
-# include <assert.h>
-# include <inttypes.h>
+#include <unistd.h>
+#include <assert.h>
+#include <inttypes.h>
+#include "nw_svr.h"
+#include "nw_clt.h"
+#include "nw_job.h"
+#include "nw_timer.h"
 
-# include "nw_svr.h"
-# include "nw_clt.h"
-# include "nw_job.h"
-# include "nw_timer.h"
-
-# include "ut_log.h"
-# include "ut_sds.h"
-# include "ut_cli.h"
-# include "ut_misc.h"
-# include "ut_list.h"
-# include "ut_kafka.h"
-# include "ut_signal.h"
-# include "ut_config.h"
-# include "ut_define.h"
-# include "ut_decimal.h"
-# include "ut_rpc_clt.h"
-# include "ut_rpc_svr.h"
-# include "ut_rpc_cmd.h"
+#include "ut_log.h"
+#include "ut_sds.h"
+#include "ut_cli.h"
+#include "ut_misc.h"
+#include "ut_list.h"
+#include "ut_kafka.h"
+#include "ut_signal.h"
+#include "ut_config.h"
+#include "ut_define.h"
+#include "ut_decimal.h"
+#include "ut_rpc_clt.h"
+#include "ut_rpc_svr.h"
+#include "ut_rpc_cmd.h"
 
 struct settings {
     bool debug;
